a10633a2ed7988380afe3fd4ef5199e24355f105
Brian D. Shapiro <briand.shapiro@Brians-MacBook-Pro.local>
2020-10-18 18:37:22 -0400

    create & view deck features functioning

--- app.py
+++ app.py
@@ -2,10 +2,13 @@ import requests
 import pdb
 import os
 
+import mtgsdk
+
+# from mtgsdk import Type
 from flask import Flask, session, request, render_template, redirect, g, flash
 from flask_debugtoolbar import DebugToolbarExtension
 from models import db, connect_db, User, Friendship, Message, Card, Bookmark, Deck, CardDeck, Post
-from forms import LoginForm, RegisterForm
+from forms import LoginForm, RegisterForm, TypeForm, PowerForm, ToughnessForm, DeckForm
 
 
 app = Flask(__name__)
@@ -23,6 +26,18 @@ connect_db(app)
 toolbar = DebugToolbarExtension(app)
 
 CURR_USER_KEY = 'curr-user'
+
+TYPES = mtgsdk.Type.all()
+
+
+def do_logout():
+    """Logout user."""
+
+    if CURR_USER_KEY in session:
+
+        user = User.query.get(session[CURR_USER_KEY])
+        del session[CURR_USER_KEY]
+        flash(f"Goodbye, {user.username}!")
 
 
 @app.before_request
@@ -96,10 +111,54 @@ def register():
 @app.route('/home')
 def show_homepage():
     cards = Card.query.all()
-    return render_template('home.html', cards=cards)
+
+    type_form = TypeForm()
+    type_form.card_type.choices = TYPES
+
+    power_form = PowerForm()
+    toughness_form = ToughnessForm()
+
+    return render_template('home.html', cards=cards, type_form=type_form, power_form=power_form, toughness_form=toughness_form)
 
 
 def check_confirmed_pwd(pwd, confirmed_pwd):
     if pwd != confirmed_pwd:
         flash('Passwords must match - please try again.', 'danger')
         return redirect('/register')
+
+
+@app.route('/decks')
+def view_decks():
+    decks = Deck.query.all()
+    return render_template('decks.html', decks=decks)
+
+
+@app.route('/decks/<int:deck_id>')
+def show_deck(deck_id):
+    deck = Deck.query.get(deck_id)
+    return render_template('deck.html')
+
+
+@app.route('/new', methods=['GET', 'POST'])
+def create_deck():
+
+    if g.user:
+        form = DeckForm()
+        form.deck_type.choices = ['Standard', 'Commander']
+
+        if form.validate_on_submit():
+
+            deck = Deck(deck_name=form.deck_name.data,
+                        deck_type=form.deck_type.data, username=session[CURR_USER_KEY])
+            db.session.add(deck)
+            db.session.commit()
+
+            return redirect('/decks')
+        return render_template('new_deck.html', form=form)
+    return redirect('/')
+
+
+@app.route('/logout')
+def logout():
+    do_logout()
+    return redirect('/login')
--- forms.py
+++ forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField
+from wtforms import StringField, PasswordField, IntegerField, RadioField, SelectField
 from wtforms.validators import InputRequired, Length
+from wtforms.widgets import CheckboxInput, ListWidget
 
 
 class LoginForm(FlaskForm):
@@ -10,9 +11,31 @@ class LoginForm(FlaskForm):
 
 
 class RegisterForm(FlaskForm):
-    email = StringField('Email', validators=[InputRequired(), ])
+    email = StringField('Email', validators=[InputRequired()])
     username = StringField("Username", validators=[InputRequired()])
     password = PasswordField("Password", validators=[
                              InputRequired(), Length(min=8)])
     confirmed_password = PasswordField(
         "Confirm Password", validators=[InputRequired()])
+
+
+class DeckForm(FlaskForm):
+    deck_name = StringField('Name', validators=[InputRequired()])
+    deck_type = SelectField('Deck Type')
+
+
+class TypeForm(FlaskForm):
+    card_type = RadioField('Type', option_widget=CheckboxInput())
+
+
+class PowerForm(FlaskForm):
+    power_conditionals = RadioField('Power', option_widget=CheckboxInput(),
+                                    choices=["Less than", "Equal to", "Greater than"])
+    power = IntegerField('Power', default=0)
+
+
+class ToughnessForm(FlaskForm):
+
+    toughness_conditionals = RadioField('Toughness', option_widget=CheckboxInput(),
+                                        choices=["Less than", "Equal to", "Greater than"])
+    toughness = IntegerField('Toughness', default=0)
